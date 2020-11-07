dc1e11b02fb7baacac382ebb98faff4c72c709c9
Connor Hutchinson <workhutchc@gmail.com>
2020-11-07 09:02:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz(num)
   if num % 3 == 0
     puts "Fizz"
-  else if num % 5 == 0
+  elsif num % 5 == 0
     puts "Buzz"
-  else if num % 3 && num % 5
+  elsif num % 3 && num % 5
     puts "FizzBuzz"
   else
     return nil
